a59309b2e452554c758ea5425975a5a27eed36d5
sophia <sophiabaldonado@gmail.com>
2018-06-13 20:36:41 -0700

    refactorrrrrr;

---
+++ config.lua
@@ -0,0 +1,15 @@
+local config = {
+	enablePoly = false
+	tools = {
+		hover = {
+			translate = require 'translate',
+			rotate = require 'rotate',
+			scale = require 'scale'
+		}
+	}
+
+
+}
+
+
+return config
