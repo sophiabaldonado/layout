38de4155bdc1f175c37f33b2fef41b6d0536f98b
sophia <sophiabaldonado@gmail.com>
2018-08-27 22:36:55 -0700

    make a model, given a name and a format;

--- poly.lua
+++ poly.lua
@@ -1,72 +1,85 @@
 local http = require 'luajit-request'
 local json = require 'json'
 local pprint = require 'pretty-print'
+local fs = lovr.filesystem
 
 local poly = {}
 
 function poly:init()
 	self.thumbnails = {}
+	self.formats = {}
 	self.models = {}
+	self.assets = {}
 
 	local list = json.decode(http.send('https://poly.googleapis.com/v1/assets?category=food&orderBy=BEST&pageSize=5&key=APIKEY').body)
 
 	self:makeThumbnails(list)
-	self:makeModels(list)
+	self:makeFormats(list)
+	polyModel = self:getModel(self.assets[1].name, self.assets[1].format)
 end
 
-function poly:makeModels(list)
-	local bestTypes = { 'GLTF2', 'GLTF', 'OBJ', 'FBX' }
+function poly:makeFormats(list)
+	local bestTypes = { 'OBJ', 'FBX' } -- { 'GLTF2', 'GLTF', 'OBJ', 'FBX' }
 	local extensions = { GLTF2 = 'gltf', GLTF = 'gltf',  OBJ = 'obj',  FBX = 'fbx' }
 
-	for i = 1, #list.assets do
+	for i, asset in ipairs(list.assets) do
+		self.assets[i] = {}
+		self.assets[i].name = asset.name
+		self.assets[i].displayName = asset.displayName
 		local format
-		for j = 1, #list.assets[i].formats do
-			local formats = list.assets[i].formats
+		local formats = asset.formats
+		for j = 1, #formats do
 			formats[formats[j].formatType] = formats[j]
 		end
 
-		for j = 1, #bestTypes do
-			local bestType = list.assets[i].formats[bestTypes[j]]
-			if bestType ~= nil then
+		for j, type in ipairs(bestTypes) do
+			local bestType = formats[type]
+			if bestType then
 				format = bestType
 				break
 			end
 		end
 
-		if format ~= nil then
-			local assetBlob = lovr.data.newBlob(http.send(format.root.url).body, list.assets[i].name..'.'..extensions[format.formatType])
-			local pngBlob = lovr.data.newBlob(http.send(format.resources.url).body)
-			local assetMat = lovr.graphics.newMaterial(lovr.graphics.newTexture(pngBlob))
-			table.insert(self.models, lovr.graphics.newModel(assetBlob))
+		if format then
+			self.assets[i].format = format
+			self.formats[asset.name] = format
 		end
 	end
 end
 
-function poly:makeThumbnails(list)
-	for i = 1, #list.assets do
-		self.thumbnails[list.assets[i].name] = {}
+function poly:getModel(name, format)
+	if self.models[format] then return self.models[format] end
+	local basePath = name..'/'
 
-		local thumbPath = list.assets[i].thumbnail.url
+	fs.createDirectory(basePath)
+	fs.write(basePath..format.root.relativePath, http.send(format.root.url).body)
+	for i, resource in ipairs(format.resources) do
+		fs.write(basePath..resource.relativePath, http.send(resource.url).body)
+	end
+	local model = lovr.graphics.newModel(basePath..format.root.relativePath)
+	self.models[format] = model
+	return model
+end
+
+function poly:makeThumbnails(list)
+	for i, asset in ipairs(list.assets) do
+		self.thumbnails[asset.name] = {}
+
+		local thumbPath = asset.thumbnail.url
 		local thumbBlob = lovr.data.newBlob(http.send(thumbPath).body)
 		local thumbMat = lovr.graphics.newMaterial(lovr.graphics.newTexture(thumbBlob))
-		self.thumbnails[list.assets[i].name].material = thumbMat
+		self.thumbnails[asset.name].material = thumbMat
 	end
 end
 
 function poly:draw()
-	-- lovr.graphics.sphere(0, 0, 0, .1)
-	-- polyModel:draw(0, 0, -5, .3)
+	polyModel:draw(0, 1, -2, .25)
 
-	for i = 1, #self.models do
-		print(self.models[i])
-		self.models[i]:draw(-3 + (1.25 * i), 1, -2, .15)
+	local j = 0
+	for k, v in pairs(self.thumbnails) do
+			lovr.graphics.plane(v.material, -3 + (1 * j), 2, -2)
+			j = j + 1
 	end
-
-	-- local i = 0
-	-- for k,v in pairs(self.thumbnails) do
-	-- 		lovr.graphics.plane(v.material, -3 + (1.25 * i), 2, -2)
-	-- 		i = i + 1
-	-- end
 end
 
 return poly
