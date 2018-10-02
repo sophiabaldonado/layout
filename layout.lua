41986c8428a7cfddfe6fd1dea187100fdcbce4c2
bjorn <tie.372@gmail.com>
2018-10-02 11:55:27 -0700

    Organization;

--- layout.lua
+++ layout.lua
@@ -4,17 +4,11 @@ local json = require 'cjson'
 local base = (...):match('^(.*[%./])[^%.%/]+$') or ''
 local dot = base:match('%.') and '.' or '/'
 
-local function angle(x1, y1, x2, y2)
-  return math.atan2(y2 - y1, x2 - x1)
-end
-
-local function distance(x1, y1, x2, y2)
-  local dx, dy = x2 - x1, y2 - y1
-  return math.sqrt(dx * dx + dy * dy)
-end
-
 local layout = {}
 
+----------------
+-- Callbacks
+----------------
 function layout:init()
   self.isDirty = false
   self.lastChange = nil
@@ -49,7 +43,7 @@ function layout:init()
 end
 
 function layout:update(dt)
-  self:checkSave()
+  self:autosave()
 
   -- Calculate hover state
   for i, entity in ipairs(self.entities) do
@@ -74,10 +68,13 @@ function layout:draw()
   self:eachTool('draw')
 end
 
+----------------
+-- Controllers
+----------------
 function layout:controllerpressed(controller, button)
   if button == 'touchpad' then
     local touchx, touchy = controller:getAxis('touchx'), controller:getAxis('touchy')
-    local angle, distance = angle(0, 0, touchx, touchy), distance(0, 0, touchx, touchy)
+    local angle, distance = math.atan2(touchy, touchx), math.sqrt(touchx * touchx + touchy * touchy)
     local threshold = 0
     while angle < 0 do angle = angle + 2 * math.pi end
     if distance >= threshold then
@@ -120,6 +117,9 @@ function layout:getOtherController(controller)
   return self.controllers[controller]
 end
 
+----------------
+-- Actions
+----------------
 function layout:setActiveActions()
   self.actions.up = function() self.axisLock.y = not self.axisLock.y end
   self.actions.left = function() self.axisLock.x = not self.axisLock.x end
@@ -180,17 +180,10 @@ function layout:setHoverActions()
 end
 
 function layout:setDefaultActions()
-  local function file()
-    --self:saveAsCopy()
-  end
-
-  local function undo() end
-  local function redo() end
-
   self.actions.up = function() end
-  self.actions.left = function() undo() end
-  self.actions.right = function() redo() end
-  self.actions.down = function() file() end
+  self.actions.left = function() end
+  self.actions.right = function() end
+  self.actions.down = function() end
 end
 
 function layout:setActionTexture(name)
@@ -214,91 +207,66 @@ function layout:drawActionUI()
   end
 end
 
-function layout:drawCursors()
-  for _, controller in ipairs(self.controllers) do
-    local x, y, z = self:cursorPos(controller)
-
-    if (self.activeColor ~= self.colors.default) then
-      lovr.graphics.setColor(self.activeColor)
-      lovr.graphics.sphere(x, y, z, .005)
-      lovr.graphics.setColor(self.colors.default)
-    else
-      lovr.graphics.cube('fill', x, y, z, .01)
-    end
-  end
-end
-
-function layout:drawEntities()
-  for _, entity in ipairs(self.entities) do
-    local model = self.models[entity.kind]
-    local minx, maxx, miny, maxy, minz, maxz = model:getAABB()
-    local cx, cy, cz = (minx + maxx) / 2 * entity.scale, (miny + maxy) / 2 * entity.scale, (minz + maxz) / 2 * entity.scale
-    lovr.graphics.push()
-    lovr.graphics.translate(entity.x + cx, entity.y + cy, entity.z + cz)
-    lovr.graphics.rotate(entity.angle, entity.ax, entity.ay, entity.az)
-    lovr.graphics.translate(-entity.x - cx, -entity.y - cy, -entity.z - cz)
-    model:draw(entity.x, entity.y, entity.z, entity.scale)
-    lovr.graphics.pop()
-
-    self:drawEntityUI(entity)
-  end
-end
-
-function layout:drawEntityUI(entity)
-  local r, g, b = unpack(self.activeColor)
-  local alpha = .392
-  local highA = .784
-  if entity.hovered then alpha = highA end
-
-  local model = self.models[entity.kind]
-  local minx, maxx, miny, maxy, minz, maxz = model:getAABB()
-  local w, h, d = (maxx - minx) * entity.scale, (maxy - miny) * entity.scale, (maxz - minz) * entity.scale
-  local cx, cy, cz = (maxx + minx) / 2 * entity.scale, (maxy + miny) / 2 * entity.scale, (maxz + minz) / 2 * entity.scale
-  lovr.graphics.push()
-  lovr.graphics.translate(entity.x, entity.y, entity.z)
-  lovr.graphics.translate(cx, cy, cz)
-  lovr.graphics.rotate(entity.angle, entity.ax, entity.ay, entity.az)
-  lovr.graphics.translate(-cx, -cy, -cz)
-  if entity.hovered then
-    if entity.locked then
-      r, g, b = unpack(self.colors.orange)
-    end
-    lovr.graphics.setColor(r, g, b, alpha)
-  else
-    r, g, b = unpack(self.colors.default)
-    lovr.graphics.setColor(r, g, b, alpha)
-  end
-  lovr.graphics.box('line', cx, cy, cz, w, h, d)
-  lovr.graphics.pop()
-  for axis, locked in pairs(self.axisLock) do
-    if locked then
-      lovr.graphics.setLineWidth(2)
-      local x, y, z = entity.x, entity.y, entity.z
-      if axis == 'x' then
-        local r, g, b = unpack(self.colors.red)
-        lovr.graphics.setColor(r, g, b, highA)
-        lovr.graphics.line(x - 10, y, z, x + 10, y, z)
-      elseif axis == 'y' then
-        local r, g, b = unpack(self.colors.green)
-        lovr.graphics.setColor(r, g, b, highA)
-        lovr.graphics.line(x, y - 10, z, x, y + 10, z)
-      elseif axis == 'z' then
-        local r, g, b = unpack(self.colors.blue)
-        lovr.graphics.setColor(r, g, b, highA)
-        lovr.graphics.line(x, y, z - 10, x, y, z + 10)
-      end
-      lovr.graphics.setLineWidth(1)
-    end
-  end
-  lovr.graphics.setColor(self.colors.default)
-end
-
+----------------
+-- Cursors
+----------------
 function layout:cursorPos(controller)
   local x, y, z = controller:getPosition()
   local ox, oy, oz = lovr.math.orientationToDirection(controller:getOrientation())
   return x + ox * scale, y + oy * scale, z + oz * scale
 end
 
+function layout:drawCursors()
+  for _, controller in ipairs(self.controllers) do
+    local x, y, z = self:cursorPos(controller)
+    lovr.graphics.setColor(1, 1, 1)
+    lovr.graphics.cube('fill', x, y, z, .01)
+  end
+end
+
+local transform = lovr.math.newTransform()
+function layout:isHoveredByController(entity, controller)
+  if not controller then return false end
+  local function addMinimumBuffer(minx, maxx, miny, maxy, minz, maxz, scale)
+    local w, h, d = (maxx - minx) * scale, (maxy - miny) * scale, (maxz - minz) * scale
+    if w <= .005 then minx = minx + .005 end
+    if h <= .005 then miny = miny + .005 end
+    if d <= .005 then minz = minz + .005 end
+    return minx, maxx, miny, maxy, minz, maxz
+  end
+
+  local t = entity
+  local model = self.models[t.kind]
+  local minx, maxx, miny, maxy, minz, maxz = model:getAABB()
+  minx, maxx, miny, maxy, minz, maxz = addMinimumBuffer(minx, maxx, miny, maxy, minz, maxz, t.scale)
+  local cx, cy, cz = (minx + maxx) / 2 * t.scale, (miny + maxy) / 2 * t.scale, (minz + maxz) / 2 * t.scale
+  minx, maxx, miny, maxy, minz, maxz = t.x + minx * t.scale, t.x + maxx * t.scale, t.y + miny * t.scale, t.y + maxy * t.scale, t.z + minz * t.scale, t.z + maxz * t.scale
+  transform:origin()
+  transform:translate(t.x, t.y, t.z)
+  transform:translate(cx, cy, cz)
+  transform:rotate(-t.angle, t.ax, t.ay, t.az)
+  transform:translate(-cx, -cy, -cz)
+  local x, y, z = self:cursorPos(controller)
+  x, y, z = transform:transformPoint(x - t.x, y - t.y, z - t.z)
+  return x >= minx and x <= maxx and y >= miny and y <= maxy and z >= minz and z <= maxz
+end
+
+function layout:getClosestEntity(controller)
+  local x, y, z = self:cursorPos(controller)
+  local minDistance, closestEntity = math.huge, nil
+  for _, entity in pairs(self.entities) do
+    local d = (x - entity.x) ^ 2 + (y - entity.y) ^ 2 + (z - entity.z) ^ 2
+    if d < minDistance and self:isHoveredByController(entity, controller) then
+      minDistance = d
+      closestEntity = entity
+    end
+  end
+  return closestEntity, math.sqrt(minDistance)
+end
+
+----------------
+-- Entities
+----------------
 function layout:addEntity(kind, x, y, z, scale, angle, ax, ay, az)
   table.insert(self.entities, {
     locked = false,
@@ -329,77 +297,87 @@ function layout:removeEntity(entity)
   end
 end
 
-function layout:loadModels()
-  self.models = {}
+function layout:drawEntities()
+  for _, entity in ipairs(self.entities) do
+    local model = self.models[entity.kind]
+    local minx, maxx, miny, maxy, minz, maxz = model:getAABB()
+    local cx, cy, cz = (minx + maxx) / 2 * entity.scale, (miny + maxy) / 2 * entity.scale, (minz + maxz) / 2 * entity.scale
+    lovr.graphics.push()
+    lovr.graphics.translate(entity.x + cx, entity.y + cy, entity.z + cz)
+    lovr.graphics.rotate(entity.angle, entity.ax, entity.ay, entity.az)
+    lovr.graphics.translate(-entity.x - cx, -entity.y - cy, -entity.z - cz)
+    model:draw(entity.x, entity.y, entity.z, entity.scale)
+    lovr.graphics.pop()
 
-  local path = 'models'
-  for i, file in ipairs(lovr.filesystem.getDirectoryItems(path)) do
-    if file:match('%.obj$') or file:match('%.gltf$') or file:match('%.fbx$') then
-      if i > 5 then return end
-      local id = file:gsub('%.%a+$', '')
-      self.models[id] = lovr.graphics.newModel(path .. '/' .. file)
-      table.insert(self.models, id)
-    end
+    self:drawEntityUI(entity)
   end
 end
 
-function layout:getClosestEntity(controller)
-  local x, y, z = controller:getPosition()
-  local minDistance, closestEntity = math.huge, nil
-  for _, entity in pairs(self.entities) do
-    local d = (x - entity.x) ^ 2 + (y - entity.y) ^ 2 + (z - entity.z) ^ 2
-    if d < minDistance and self:isHoveredByController(entity, controller) then
-      minDistance = d
-      closestEntity = entity
-    end
-  end
-  return closestEntity, math.sqrt(minDistance)
-end
+function layout:drawEntityUI(entity)
+  local r, g, b = unpack(self.activeColor)
+  local alpha = .392
+  local highA = .784
+  if entity.hovered then alpha = highA end
 
-local transform = lovr.math.newTransform()
-function layout:isHoveredByController(entity, controller)
-  if not controller then return false end
-  local function addMinimumBuffer(minx, maxx, miny, maxy, minz, maxz, scale)
-    local w, h, d = (maxx - minx) * scale, (maxy - miny) * scale, (maxz - minz) * scale
-    if w <= .005 then minx = minx + .005 end
-    if h <= .005 then miny = miny + .005 end
-    if d <= .005 then minz = minz + .005 end
-    return minx, maxx, miny, maxy, minz, maxz
-  end
-
-  local t = entity
-  local model = self.models[t.kind]
+  local model = self.models[entity.kind]
   local minx, maxx, miny, maxy, minz, maxz = model:getAABB()
-  minx, maxx, miny, maxy, minz, maxz = addMinimumBuffer(minx, maxx, miny, maxy, minz, maxz, t.scale)
-  local cx, cy, cz = (minx + maxx) / 2 * t.scale, (miny + maxy) / 2 * t.scale, (minz + maxz) / 2 * t.scale
-  minx, maxx, miny, maxy, minz, maxz = t.x + minx * t.scale, t.x + maxx * t.scale, t.y + miny * t.scale, t.y + maxy * t.scale, t.z + minz * t.scale, t.z + maxz * t.scale
-  transform:origin()
-  transform:translate(t.x, t.y, t.z)
-  transform:translate(cx, cy, cz)
-  transform:rotate(-t.angle, t.ax, t.ay, t.az)
-  transform:translate(-cx, -cy, -cz)
-  local x, y, z = self:cursorPos(controller)
-  x, y, z = transform:transformPoint(x - t.x, y - t.y, z - t.z)
-  return x >= minx and x <= maxx and y >= miny and y <= maxy and z >= minz and z <= maxz
-end
+  local w, h, d = (maxx - minx) * entity.scale, (maxy - miny) * entity.scale, (maxz - minz) * entity.scale
+  local cx, cy, cz = (maxx + minx) / 2 * entity.scale, (maxy + miny) / 2 * entity.scale, (maxz + minz) / 2 * entity.scale
 
-function layout:checkSave()
-  if self.isDirty and self.lastChange and lovr.timer.getTime() - self.lastChange > 3 then
-    --self:save()
-    self.isDirty = false
+  lovr.graphics.push()
+  lovr.graphics.translate(entity.x, entity.y, entity.z)
+  lovr.graphics.translate(cx, cy, cz)
+  lovr.graphics.rotate(entity.angle, entity.ax, entity.ay, entity.az)
+  lovr.graphics.translate(-cx, -cy, -cz)
+  if entity.hovered then
+    if entity.locked then
+      r, g, b = unpack(self.colors.orange)
+    end
+    lovr.graphics.setColor(r, g, b, alpha)
+  else
+    r, g, b = unpack(self.colors.default)
+    lovr.graphics.setColor(r, g, b, alpha)
   end
+  lovr.graphics.box('line', cx, cy, cz, w, h, d)
+  lovr.graphics.pop()
+
+  for axis, locked in pairs(self.axisLock) do
+    if locked then
+      lovr.graphics.setLineWidth(2)
+      local x, y, z = entity.x, entity.y, entity.z
+      if axis == 'x' then
+        local r, g, b = unpack(self.colors.red)
+        lovr.graphics.setColor(r, g, b, highA)
+        lovr.graphics.line(x - 10, y, z, x + 10, y, z)
+      elseif axis == 'y' then
+        local r, g, b = unpack(self.colors.green)
+        lovr.graphics.setColor(r, g, b, highA)
+        lovr.graphics.line(x, y - 10, z, x, y + 10, z)
+      elseif axis == 'z' then
+        local r, g, b = unpack(self.colors.blue)
+        lovr.graphics.setColor(r, g, b, highA)
+        lovr.graphics.line(x, y, z - 10, x, y, z + 10)
+      end
+      lovr.graphics.setLineWidth(1)
+    end
+  end
+
+  lovr.graphics.setColor(self.colors.default)
 end
 
-function layout:dirty()
-  self.isDirty = true
-  self.lastChange = lovr.timer.getTime()
-end
+----------------
+-- IO
+----------------
+function layout:load(filename)
+  self.filename = filename
+  local path = lovr.filesystem.isFile('levels/'..filename..'.json') and 'levels/'..filename..'.json' or 'default.json'
+  self.data = json.decode(lovr.filesystem.read(path))
 
-function layout:nextFilename(filename)
-  local function versionFilename(name, version) return name .. '-' .. string.format('%02d', version) end
-  local i = 1
-  while lovr.filesystem.isFile('levels/' .. versionFilename(filename, i) .. '.json') do i = i + 1 end
-  return versionFilename(filename, i) -- TODO: make this not return 'mujugarden-02-01'
+  if self.data.entities then
+    for _, entity in ipairs(self.data.entities) do
+      self:addEntity(entity.kind, unpack(entity.transform))
+    end
+  end
 end
 
 --[[
@@ -426,20 +404,43 @@ function layout:saveAsCopy()
   self.filename = self.filename and self:nextFilename(self.filename) or nil
   self:save()
 end
+
+function layout:nextFilename(filename)
+  local function versionFilename(name, version) return name .. '-' .. string.format('%02d', version) end
+  local i = 1
+  while lovr.filesystem.isFile('levels/' .. versionFilename(filename, i) .. '.json') do i = i + 1 end
+  return versionFilename(filename, i) -- TODO: make this not return 'mujugarden-02-01'
+end
 ]]
 
-function layout:load(filename)
-  self.filename = filename
-  local path = lovr.filesystem.isFile('levels/'..filename..'.json') and 'levels/'..filename..'.json' or 'default.json'
-  self.data = json.decode(lovr.filesystem.read(path))
+function layout:autosave()
+  if self.isDirty and self.lastChange and lovr.timer.getTime() - self.lastChange > 3 then
+    --self:save()
+    self.isDirty = false
+  end
+end
 
-  if self.data.entities then
-    for _, entity in ipairs(self.data.entities) do
-      self:addEntity(entity.kind, unpack(entity.transform))
+function layout:dirty()
+  self.isDirty = true
+  self.lastChange = lovr.timer.getTime()
+end
+
+function layout:loadModels()
+  self.models = {}
+  local path = 'models'
+  for i, file in ipairs(lovr.filesystem.getDirectoryItems(path)) do
+    if file:match('%.obj$') or file:match('%.gltf$') or file:match('%.fbx$') then
+      if i > 5 then return end
+      local id = file:gsub('%.%a+$', '')
+      self.models[id] = lovr.graphics.newModel(path .. '/' .. file)
+      table.insert(self.models, id)
     end
   end
 end
 
+----------------
+-- Tools
+----------------
 function layout:eachTool(action, ...)
   for _, tool in ipairs(self.tools) do
     if tool[action] then tool[action](tool, ...) end
