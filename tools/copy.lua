124292443f256925ed3a59fb87c72426acf5dcec
bjorn <tie.372@gmail.com>
2018-10-19 15:29:06 -0700

    Copy tool use use;

--- tools/copy.lua
+++ tools/copy.lua
@@ -1,17 +1,12 @@
 local Copy = {}
 
-Copy.direction = 'up'
+Copy.context = 'hover'
+Copy.button = 'up'
 
-function Copy:controllerpressed(controller, button)
-  if button ~= 'touchpad' or self.layout:getTouchpadDirection(controller) ~= self.direction then return end
-
-  local entity = self.layout:getClosestHover(controller)
-  if entity and not entity.focused and not entity.locked then
-    local x, y, z = entity.x + .1, entity.y + .1, entity.z + .1
-    local scale = entity.scale
-    local angle, ax, ay, az = entity.angle, entity.ax, entity.ay, entity.az
-    self.layout:addEntity(entity.kind, x, y, z, scale, angle, ax, ay, az)
-  end
+function Copy:use(controller, entity)
+  local x, y, z = self.layout:cursorPos(controller) -- So they don't stack on top of each other
+  local scale, angle, ax, ay, az = entity.scale, entity.angle, entity.ax, entity.ay, entity.az
+  self.layout:addEntity(entity.kind, x, y, z, scale, angle, ax, ay, az)
 end
 
 return Copy
