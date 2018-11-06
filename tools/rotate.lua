e2b60bef9606b4b1a1db5125d2c7abb905806831
bjorn <tie.372@gmail.com>
2018-11-05 22:48:40 -0800

    Improve rotate;

--- tools/rotate.lua
+++ tools/rotate.lua
@@ -13,24 +13,28 @@ function Rotate:init()
   self.position = maf.vec3()
   self.lastPosition = maf.vec3()
   self.bzz = 0
+  self.axis = maf.vec3()
 end
 
 function Rotate:start(controller, entity)
   self.position:set(self.layout:getCursorPosition(controller))
+  self.position:sub(maf.vec3(entity.x, entity.y, entity.z))
   self.lastPosition:set(self.position)
   self.bzz = 0
 end
 
-local axis = maf.vec3()
+--local axis = maf.vec3()
 local orientation = maf.quat()
 local rotation = maf.quat()
 function Rotate:use(controller, entity, dt)
+  self.entity = entity
   self.position:set(self.layout:getCursorPosition(controller))
-  self.lastPosition:cross(self.position, axis)
-  local angle = self.position:distance(self.lastPosition)
+  self.position:sub(maf.vec3(entity.x, entity.y, entity.z))
+  self.axis:set(self.lastPosition:normalize()):cross(self.position:normalize()):normalize()
+  local angle = math.acos(self.position:dot(self.lastPosition))
   orientation:angleAxis(entity.angle, entity.ax, entity.ay, entity.az)
-  rotation:angleAxis(angle, axis)
-  orientation:mul(rotation)
+  rotation:angleAxis(angle, self.axis)
+  rotation:mul(orientation)
 
   self.bzz = self.bzz + angle
   if self.bzz >= .1 then
@@ -38,7 +42,7 @@ function Rotate:use(controller, entity, dt)
     self.bzz = 0
   end
 
-  entity.angle, entity.ax, entity.ay, entity.az = orientation:getAngleAxis()
+  entity.angle, entity.ax, entity.ay, entity.az = rotation:getAngleAxis()
   self.lastPosition:set(self.position)
   self.layout:dirty()
 end
