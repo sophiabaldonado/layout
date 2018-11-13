5373710e6cfb6be44ed4413734fcbb3d90ae7433
bjorn <tie.372@gmail.com>
2018-11-12 20:03:38 -0800

    Add tool icons to default tools;

--- tools/copy.lua
+++ tools/copy.lua
@@ -5,6 +5,7 @@ local Copy = {}
 Copy.name = 'Copy'
 Copy.context = 'hover'
 Copy.button = 'up'
+Copy.icon = 'copy.png'
 
 function Copy:use(controller, entity)
   local x, y, z = self.layout:getCursorPosition(controller) -- So they don't stack on top of each other
--- tools/delete.lua
+++ tools/delete.lua
@@ -5,6 +5,7 @@ local Delete = {}
 Delete.name = 'Delete'
 Delete.context = 'hover'
 Delete.button = 'right'
+Delete.icon = 'delete.png'
 
 function Delete:use(controller, entity)
   self.layout:removeEntity(entity)
